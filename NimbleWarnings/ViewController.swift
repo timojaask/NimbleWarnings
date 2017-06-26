1bda1096c060e5ad4072c7058543ad1aa5715920
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-06-26 10:13:33 -0400

    Add the demo project

---
+++ NimbleWarnings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NimbleWarnings
+//
+//  Created by Timo Jääskeläinen on 6/26/17.
+//  Copyright © 2017 timojaask. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-NimbleWarnings/Pods-NimbleWarnings-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NimbleWarnings : NSObject
+@end
+@implementation PodsDummy_Pods_NimbleWarnings
+@end
